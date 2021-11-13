47305b626b2fc30ae4d8fd82d9b7c00b32dc5871
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-11-13 18:40:04 +0100

    сделал домашку и добавил библиотеку Notify

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  body: document.body,
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onSwichThem = e => {
+  e.target.disabled = true;
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const offSwichThem = e => {
+  refs.start.disabled = false;
+
+  clearInterval(intervalId);
+};
+
+refs.start.addEventListener('click', onSwichThem);
+refs.stop.addEventListener('click', offSwichThem);
